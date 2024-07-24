aa942343a6c0b7e60097b723c6f835cf9e9e896a
Miguel Andrade <miguandr@Miguels-MacBook-Pro.fritz.box>
2024-07-24 16:53:49 +0200

    old version

---
+++ obj/utils/init.d
@@ -0,0 +1,28 @@
+obj//utils/init.o: sources//utils/init.c \
+  sources//utils/../../includes/header_mig.h \
+  includes/../libft/includes/libft.h \
+  /opt/homebrew/opt/readline/include/readline/readline.h \
+  /opt/homebrew/opt/readline/include/readline/rlstdc.h \
+  /opt/homebrew/opt/readline/include/readline/rltypedefs.h \
+  /opt/homebrew/opt/readline/include/readline/keymaps.h \
+  /opt/homebrew/opt/readline/include/readline/chardefs.h \
+  /opt/homebrew/opt/readline/include/readline/tilde.h \
+  /opt/homebrew/opt/readline/include/readline/history.h
+
+sources//utils/../../includes/header_mig.h:
+
+includes/../libft/includes/libft.h:
+
+/opt/homebrew/opt/readline/include/readline/readline.h:
+
+/opt/homebrew/opt/readline/include/readline/rlstdc.h:
+
+/opt/homebrew/opt/readline/include/readline/rltypedefs.h:
+
+/opt/homebrew/opt/readline/include/readline/keymaps.h:
+
+/opt/homebrew/opt/readline/include/readline/chardefs.h:
+
+/opt/homebrew/opt/readline/include/readline/tilde.h:
+
+/opt/homebrew/opt/readline/include/readline/history.h:
