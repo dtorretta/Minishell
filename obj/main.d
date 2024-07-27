af84bd6f25ad06463a683ad48af35549f4098427
Miguel Andrade <miguandr@Miguels-MBP.fritz.box>
2024-07-27 21:45:15 +0200

    expander added to parser

--- obj/main.d
+++
@@ -1,4 +0,0 @@
-obj//main.o: sources//main.c sources//../includes/header_mig.h \
- sources//../includes/../libft/includes/libft.h
-sources//../includes/header_mig.h:
-sources//../includes/../libft/includes/libft.h:
