01f98249f6e88c9eab3ac969a6e6ccfcd03c7359
Miguel Andrade <miguandr@Miguels-MacBook-Pro.fritz.box>
2024-08-06 21:23:52 +0200

    pipe imprime una vez

--- sources/executor/executor_heredoc.c
+++ sources/executor/executor_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: miguandr <miguandr@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 19:27:45 by miguandr          #+#    #+#             */
-/*   Updated: 2024/08/06 21:18:56 by miguandr         ###   ########.fr       */
+/*   Updated: 2024/08/06 21:23:42 by miguandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 
 //crea el contenido de heredoc_content pero no hace nada si dentro hay funciones
-int ft_heredoc (t_parser *commands, t_mshell *minishell)
+int	ft_heredoc(t_parser *commands, t_mshell *minishell)
 {
-	char *input_line;
-	char *delimiter;
-	int file;
-	char *expanded_line;
+	char	*input_line;
+	char	*delimiter;
+	int		file;
+	char	*expanded_line;
 
 	//printf("entro a heredoc");
 	delimiter = commands->redirections->str;
@@ -31,15 +31,15 @@ int ft_heredoc (t_parser *commands, t_mshell *minishell)
 	// 	close(file);
 	// 	return (handle_error(minishell, 8));
 	// }
-	while(1) //a;adir expander aca (ver si esta bien el que a;adi) // ver manejo de quotes, supuestamente habria que eliminarlas
+	while (1)//a;adir expander aca (ver si esta bien el que a;adi) // ver manejo de quotes, supuestamente habria que eliminarlas
 	{
 		input_line = readline("> ");
 		if (!input_line)
-			return(EXIT_FAILURE); //CAMBIAR, NO HABRIA QUE AGREGARLO EN EL LEXER TAMBIEN?
+			return (EXIT_FAILURE); //CAMBIAR, NO HABRIA QUE AGREGARLO EN EL LEXER TAMBIEN?
 		if (!ft_strncmp(input_line, delimiter, ft_strlen(delimiter)) && input_line[ft_strlen(delimiter)] == '\0') //si encuentra el limitador se termina
 		{
 			free(input_line);
-			break;
+			break ;
 		}
 		else
 		{
@@ -50,7 +50,7 @@ int ft_heredoc (t_parser *commands, t_mshell *minishell)
 		}
 	}
 	close(file);
-	return(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
 char	*generate_name(void)
